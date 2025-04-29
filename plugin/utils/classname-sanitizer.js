f3ac7a2e5e4b9fae861213295c2bc92629bb7277
Ian Proefriedt <ian@ianproux.com>
2025-04-28 20:10:11 -0400

    Added some file types to previously created files. Minor fixes throughout file.

--- plugin/utils/classname-sanitizer.js
+++ plugin/utils/classname-sanitizer.js
@@ -2,6 +2,6 @@ export function sanitizeClassName(name) {
   if (typeof name !== 'string') return '';
   return name
     .toLowerCase()
-    .trim()
+    .replace(/\s+/g, '-')
     .replace(/[^a-z0-9\-_]/g, '');
 }
